fe1d7fb7185d3c3fdf94dd10721d4ea7035ee4c0
vepalevicius <ve.palevicius@gmail.com>
2024-11-20 10:39:22 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Smooth scrolling effect for anchors
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      const offset = 80; // Altura do menu fixo (ajuste conforme necessário)
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = target.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
+});
